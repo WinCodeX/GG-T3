942b30ee20c99c609f349eec60eb76442f9d9e15
WinCodeX <glenwinterg970@gmail.com>
2025-04-04 23:21:21 +0300

    added role access for database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: {
+    admin: 0,
+    agent: 1,
+    rider: 2,
+    warehouse: 3,
+    client: 4
+  }
 end
